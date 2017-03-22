89f3e139cbc899ba527f837d16b87de7ac97a114
schmi <schmidt13@live.dk>
2017-03-22 20:10:37 +0100

    BluetoothConnectionService

--- app/src/main/java/com/example/matos/bluetoothapp/BluetoothConnectionService.java
+++ app/src/main/java/com/example/matos/bluetoothapp/BluetoothConnectionService.java
@@ -3,8 +3,11 @@ package com.example.matos.bluetoothapp;
 import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.util.Log;
 
+import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -32,10 +35,48 @@ public class BluetoothConnectionService {
         private final BluetoothServerSocket mmServerSocket;
 
         public AcceptThread(){
-            mmServerSocket tmp = null;
+            BluetoothServerSocket tmp = null;
 
-            tmp = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(appName,MY_UUID_INSECURE);
+            try{
+                tmp = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(appName,MY_UUID_INSECURE);
+                Log.d(TAG, "AcceptThread: Setting up server using: " + MY_UUID_INSECURE);
+            }catch (IOException e){
+                Log.e(TAG, "AcceptThread IOException: " + e.getMessage());
+            }
 
+            mmServerSocket = tmp;
+
+        }
+
+        public void run(){
+            Log.d(TAG, "run: AcceptThread Running.");
+
+            BluetoothSocket socket = null;
+
+            try{
+                Log.d(TAG, "run: RFCOM server socket start.....");
+                socket = mmServerSocket.accept();
+                Log.d(TAG, "run: RFCOM server socket accepted connection");
+            }catch(IOException e){
+                Log.e(TAG, "AcceptThread IOException: " + e.getMessage());
+            }
+
+            if(socket != null){
+                connected(socket, mmDevice);
+            }
+
+            Log.i(TAG, "End mAcceptThread");
+
+        }
+
+        public void cancel(){
+            Log.d(TAG, "cancel: Cencelling AcceptThread.");
+
+            try{
+                mmServerSocket.close();
+            }catch(IOException e){
+                Log.e(TAG, "cencel: Close of AcceptThread ServerSocket failed: " + e.getMessage());
+            }
 
         }
     }
