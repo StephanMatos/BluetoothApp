b57815f4a6cc0ebc168d9ab1d62792cb7debcdc4
schmi <schmidt13@live.dk>
2017-03-22 18:15:36 +0100

    BluetoothConnectionService

--- app/src/main/java/com/example/matos/bluetoothapp/BluetoothConnectionService.java
+++ app/src/main/java/com/example/matos/bluetoothapp/BluetoothConnectionService.java
@@ -1,5 +1,10 @@
 package com.example.matos.bluetoothapp;
 
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.content.Context;
+
 import java.util.UUID;
 
 /**
@@ -12,5 +17,27 @@ public class BluetoothConnectionService {
     private static final String appName = "BluetoothApp";
     private static final UUID MY_UUID_INSECURE = UUID.fromString("8ce25c0-200a-11e0-ac64-0800200c9a66");
 
+    private final BluetoothAdapter mBluetoothAdapter;
+    Context mContext;
+
+    private AcceptThread mInsecureAcceptThread;
+
+    public BluetoothConnectionService(Context context){
+        mContext = context;
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    private class AcceptThread extends Thread {
+
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread(){
+            mmServerSocket tmp = null;
+
+            tmp = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(appName,MY_UUID_INSECURE);
+
+
+        }
+    }
 
 }
