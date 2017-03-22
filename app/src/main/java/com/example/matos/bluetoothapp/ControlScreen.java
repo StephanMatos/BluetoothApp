9d672bbb090fbdb3c28361a2575e9b4dec3e0a13
Stephan Matos <Stephanmatos@gmail.com>
2017-03-22 13:35:51 +0100

    a

---
+++ app/src/main/java/com/example/matos/bluetoothapp/ControlScreen.java
@@ -0,0 +1,13 @@
+package com.example.matos.bluetoothapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_screen);
+    }
+}
--- app/src/main/java/com/example/matos/bluetoothapp/DeviceList.java
+++ app/src/main/java/com/example/matos/bluetoothapp/DeviceList.java
@@ -17,7 +17,7 @@ import android.bluetooth.BluetoothDevice;
 
 public class DeviceList extends AppCompatActivity {
 
-    //hejhej1
+
 
 
     @Override
